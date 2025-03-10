dcbf7aa13124866ae61b8741443020ef78535097
Ulfahri <ulfahri1212@gmail.com>
2025-03-10 11:47:11 +0700

    Create mahasiswa.php
    Buat modul mahasiswa

---
+++ mahasiswa.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Data Mahasiswa</title>
+    </head>
+    <body>
+        <h6>Data Mahasiswa AMP</h6>
+        <hr>
+        <h6>NIM : 11111111</h6>
+        <h6>Nama : Ulfahri</h6>
+        <a href="index.php">Kembali</a>
+    </body>
+</html>
