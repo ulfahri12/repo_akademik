ae3d56b1ddde6f238e7abf30621bb82d7da85aff
khanafatimah <khanafd338@gmail.com>
2025-03-17 10:51:41 +0700

    Update mahasiswa.php
    Menambahkan Kelas

--- mahasiswa.php
+++ mahasiswa.php
@@ -7,6 +7,7 @@
         <hr>
         <h6>NIM : 11111111</h6>
         <h6>Nama : Ulfahri</h6>
+        <h6>kelas : 23MIA</h6>
         <h6>Ketua</h6>
         <a href="index.php">Kembali</a>
     </body>
